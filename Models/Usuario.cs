6859f4ced3dc25a26e97614f0431b19bb0b96abf
EctorGabriel09 <teixeragabriel16@gmail.com>
2026-06-28 12:40:44 -0300

    Configurando Entity Framework e criando banco de dados

---
+++ Models/Usuario.cs
@@ -0,0 +1,17 @@
+namespace UniGymFitness.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Telefone { get; set; } = string.Empty;
+
+        public string Senha { get; set; } = string.Empty;
+
+        public string Plano { get; set; } = string.Empty;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using UniGymFitness.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do banco de dados MySQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,11 +19,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
