eb9cbd86f16cfac1dd1f6cdcba3179560cf0fbae
EctorGabriel09 <teixeragabriel16@gmail.com>
2026-06-30 20:52:58 -0300

    Adicionando indicadores dinâmicos ao dashboard

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using UniGymFitness.Data;
 
 namespace UniGymFitness.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.TotalUsuarios = _context.Usuarios.Count();
+
             return View();
         }
     }
